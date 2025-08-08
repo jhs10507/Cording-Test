1aab825d1fcc59edf9cddb8b086bc475ba09ac31
Hyunsu Jung <jhs880514@naver.com>
2025-08-08 10:00:45 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 4.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    
+    for (const auto& command : commands) {
+        int i = command[0] - 1;
+        int j = command[1] - 1;
+        int k = command[2] - 1;
+        
+        vector<int> temp;
+        for (int idx = i; idx <= j; ++idx) {
+            temp.push_back(array[idx]);
+        }
+        sort(temp.begin(), temp.end());
+        answer.push_back(temp[k]);
+    }
+    
+    return answer;
+}
