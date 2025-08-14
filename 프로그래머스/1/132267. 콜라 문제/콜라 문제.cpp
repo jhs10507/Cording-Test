e5e4b49ab275beac0ae4ec3cf99801ac02884a5d
Hyunsu Jung <jhs880514@naver.com>
2025-08-14 09:56:58 +0900

    [level 1] Title: 콜라 문제, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b, int n) {
+    int answer = 0;
+   
+    while (n >= a)
+    {
+        int received = (n / a) * b;
+        answer += received;
+        n = (n % a) + received;
+    }
+    
+    return answer;
+}
