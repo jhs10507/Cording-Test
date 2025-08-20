d538529936b8062dd851cfbc4a3c29183faec9f2
Hyunsu Jung <jhs880514@naver.com>
2025-08-20 10:05:19 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(vector<string> cards1, vector<string> cards2, vector<string> goal) {
+    string answer = "";
+    int c1 = -1, c2 = -1;
+    
+    for (int i = 0; i < goal.size(); ++i)
+    {
+        if (find(cards1.begin(), cards1.end(), goal[i]) != cards1.end())
+        {
+            int c1_pos = find(cards1.begin(), cards1.end(), goal[i]) - cards1.begin();
+            if (c1 > c1_pos || c1_pos - 1 != c1) return answer = "No";
+            else c1 = c1_pos;
+        }
+        else if (find(cards2.begin(), cards2.end(), goal[i]) != cards2.end())
+        {
+            int c2_pos = find(cards2.begin(), cards2.end(), goal[i]) - cards2.begin();
+            if (c2 > c2_pos || c2_pos - 1 != c2) return answer = "No";
+            else c2 = c2_pos;
+        }
+    }
+    
+    return answer = "Yes";
+}
