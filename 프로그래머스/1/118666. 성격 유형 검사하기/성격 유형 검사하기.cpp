0d5acc87a7da12de33a1433e6f4cf2ac353378a1
Hyunsu Jung <jhs880514@naver.com>
2025-09-15 09:52:23 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.07 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+string solution(vector<string> survey, vector<int> choices) {
+    
+    unordered_map<char, int> m;
+    
+    for (int i = 0; i < survey.size(); ++i)
+    {
+        int selection = choices[i];
+        
+        if (selection < 4)
+            m[survey[i][0]] += (4 - selection);
+        else if (selection > 4)
+            m[survey[i][1]] += (selection - 4);
+    }
+    
+    string answer = "";
+    answer.push_back(m['R'] >= m['T'] ? 'R' : 'T');
+    answer.push_back(m['C'] >= m['F'] ? 'C' : 'F');
+    answer.push_back(m['J'] >= m['M'] ? 'J' : 'M');
+    answer.push_back(m['A'] >= m['N'] ? 'A' : 'N');
+    
+    return answer;
+}
