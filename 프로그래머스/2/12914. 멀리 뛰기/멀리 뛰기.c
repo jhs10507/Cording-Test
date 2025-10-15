d511d42bbccf408299b80a1ff567254ffb2aeef7
Hyunsu Jung <jhs880514@naver.com>
2025-10-15 09:50:24 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+long long solution(int n) {
+    long long answer = 0;
+    int n2 = 2, n3 = 3;
+    
+    if (n <= 3) return n;
+    for (int i = 4; i <= n; ++i) {
+        answer = (n2 + n3) % 1234567;
+        n2 = n3;
+        n3 = answer;
+    }
+    
+    return answer;
+}
