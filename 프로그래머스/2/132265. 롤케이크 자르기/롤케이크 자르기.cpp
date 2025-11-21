71c5654fa9f65b012e8f37d43b7eb254f393065a
Hyunsu Jung <jhs880514@naver.com>
2025-11-21 09:44:05 +0900

    [level 2] Title: 롤케이크 자르기, Time: 54.33 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cpp
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cpp
@@ -5,17 +5,14 @@ using namespace std;
 
 int solution(vector<int> topping) {
     int answer = 0, cake;
-    
-    unordered_map <int, int> base, cmp; // base: 원본 케이크, cmp: 잘린 상태
-    
+    unordered_map <int, int> base, cmp; // 본래 케이크와 잘린 상태 변수
     for (auto c : topping) base[c]++;
-    
-    cake = base.size(); // 케이크에 올라간 토핑의 총 개수
-    
-    for (auto c : topping) { 
-        cmp[c]++; base[c]--; // 한 토핑씩 자르기
-        if (base[c] == 0) cake--; // 해당 토핑의 개수가 떨어지면 안 자른 케이크 토핑 수 --
-        if (cake == cmp.size()) answer++; // 자르지 않은 케이크 토핑 수 == 자른 케이크 토핑 수
+    cake = base.size(); // 케이크 토핑의 수
+    for (auto c : topping)
+    {
+        cmp[c]++; base[c]--;    // 한 토핑씩 자르기
+        if (base[c] == 0) cake--;   // 토핑 개수가 떨어지면 자르지 않은 케이크 토핑 수 --
+        if (cake == cmp.size()) answer++; // 안 자른 케이크 토핑 수와 자른 케이크 토핑 수가 같으면
     }
     return answer;
 }
