fb1da913abbd2a04816aa73af21e924e3b889cfc
Hyunsu Jung <jhs880514@naver.com>
2025-11-24 09:59:24 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 8.55 ms, Memory: 4.02 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.cpp
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.cpp
@@ -1,64 +1,56 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 #include <cmath>
 
 using namespace std;
 
-// 소수 판별 함수
-bool isPrime(long long num) {
-    if (num < 2) return false;
-    if (num == 2) return true;
-    if (num % 2 == 0) return false;
-    
-    // sqrt(num)까지만 확인
-    long long sqrtNum = sqrt(num);
-    for (long long i = 3; i <= sqrtNum; i += 2) {
-        if (num % i == 0) return false;
+string num;
+
+void changeNum(int n, int k) {
+    while (n > 0) {
+        num += to_string(n % k);
+        n /= k;
+    }
+    reverse(num.begin(), num.end());
+}
+
+bool checkPrime(long long n) {
+    if (n == 0 || n == 1) {
+        return false;
+    }
+    for (int i = 2; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            return false;
+        }
     }
     return true;
 }
 
-// n을 k진수로 변환
-string convertToBase(int n, int k) {
-    string result = "";
-    while (n > 0) {
-        result = to_string(n % k) + result;
-        n /= k;
+int findAnswer() {
+    int answer = 0;
+    string str = "";
+    for (auto c : num) {
+        if (c == '0') {
+            if (str.length() == 0) continue;
+            if (checkPrime(stol(str)) == true) {
+                answer++;
+            }
+            str = "";
+        } else {
+            str += c;
+        }
     }
-    return result;
+    
+    if (str.length() != 0) {
+        if (checkPrime(stol(str)) == true) answer++;
+    }
+    return answer;
 }
 
 int solution(int n, int k) {
-    // n을 k진수로 변환
-    string converted = convertToBase(n, k);
-    
-    // 0을 기준으로 분리하여 소수 개수 세기
-    int count = 0;
-    string temp = "";
-    
-    for (int i = 0; i < converted.length(); i++) {
-        if (converted[i] == '0') {
-            // 0을 만나면 현재까지 모은 숫자가 소수인지 확인
-            if (!temp.empty()) {
-                long long num = stoll(temp);
-                if (isPrime(num)) {
-                    count++;
-                }
-                temp = "";
-            }
-        } else {
-            // 0이 아니면 숫자를 모음
-            temp += converted[i];
-        }
-    }
-    
-    // 마지막 남은 숫자 확인
-    if (!temp.empty()) {
-        long long num = stoll(temp);
-        if (isPrime(num)) {
-            count++;
-        }
-    }
-
-    return count;
+    int answer = -1;
+    changeNum(n, k);
+    answer = findAnswer();
+    return answer;
 }
