06cf7527f643a11b5c3416df1d16f73d99b36043
Hyunsu Jung <jhs880514@naver.com>
2025-11-24 09:50:23 +0900

    [level 2] Title: 주차 요금 계산, Time: 1.12 ms, Memory: 3.98 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.cpp
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.cpp
@@ -3,7 +3,6 @@
 #include <map>
 #include <sstream>
 #include <cmath>
-
 using namespace std;
 
 int time_diff(string in, string out) {
@@ -22,28 +21,27 @@ vector<int> solution(vector<int> fees, vector<string> records) {
     map<string, vector<string>> m;
     
     stringstream ss;
-    for(auto record: records) {
+    for (auto record: records) {
         ss.str(record);
-        string time, number, status; 
-        ss >> time >> number >> status;
+        string time, number, status; ss >> time >> number >> status;
         
         m[number].push_back(time);
         ss.clear();
     }
     
-    for(auto it: m) {
-        if(it.second.size() & 1) // 주차 내역 홀수면 "23:59" 추가
+    for (auto it : m) {
+        if (it.second.size() & 1)
             it.second.push_back("23:59");
         
         vector<string> info = it.second;
         int total = 0;
-        for(int i = 0; i<info.size() - 1; i += 2) {
+        for (int i = 0; i < info.size() - 1; i += 2) {
             total += time_diff(info[i], info[i + 1]);
         }
         
         int price = fees[1];
-        if(total > fees[0]) {
-            price += ceil((total-fees[0]) / (double)fees[2]) * fees[3];
+        if (total > fees[0]) {
+            price += ceil((total - fees[0]) / (double)fees[2]) * fees[3];
         }
         
         answer.push_back(price);
