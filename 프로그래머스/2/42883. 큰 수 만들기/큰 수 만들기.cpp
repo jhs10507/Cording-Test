46ba9943d64b17a44a35e7c8417ce8858e6b24e7
Hyunsu Jung <jhs880514@naver.com>
2025-12-01 09:16:27 +0900

    [level 2] Title: 큰 수 만들기, Time: 2426.70 ms, Memory: 5.57 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string number, int k) {
+    string answer = "";
+    
+    int len = number.length();
+    
+    for (int j = 0; j < len; j++) {
+        if (answer.length() >= (len - k)) break;
+        
+        int cursor = 0;
+        char temp = number[cursor];
+        
+        for (int i = 0; i <= k; i++) {
+            if (temp < number[i]) {
+                temp = number[i];
+                cursor = i;
+            }
+        }
+        answer += temp;
+        number.erase(0, cursor + 1);
+    
+        k -= cursor;
+    
+        if (k <= 0) {
+            answer += number;
+            break;
+        }
+    }
+    return answer;
+}
