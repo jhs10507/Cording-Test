d518c35b00aedf0e5f1f44a1aee5fab15fd7c921
Hyunsu Jung <jhs880514@naver.com>
2025-12-03 09:28:46 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 5.29 ms, Memory: 36.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> sequence, int k) {
+    vector<int> answer;
+    int s = 0, e = 0;
+    int sum = sequence[0];  // 부분 수열의 합
+    int subLen = sequence.size() + 1;   // 부분 수열의 길이
+    pair<int, int> result;  // 부분 수열의 시작 인덱스와 마지막 인덱스를 담은 객체
+    
+    while (s <= e && e < sequence.size()) {
+        if (sum < k) sum += sequence[++e];
+        else if (sum == k) {
+            if (e-s+1 < subLen) {   // 길이가 더 짧은 수열이면
+                subLen = e-s+1;
+                result = {s, e};
+            } else if (e-s+1 == subLen) {
+                if (s < result.first) {     // 시작 인덱스가 더 작으면
+                    result = {s, e};
+                }
+            }
+            
+            sum -= sequence[s++];
+        }
+        else sum -= sequence[s++];
+    }
+    
+    return answer = {result.first, result.second};
+}
