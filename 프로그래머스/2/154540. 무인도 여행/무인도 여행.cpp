d8b2e6939f454c810e1816805e0fe11d964f223f
Hyunsu Jung <jhs880514@naver.com>
2025-12-05 09:52:17 +0900

    [level 2] Title: 무인도 여행, Time: 1.68 ms, Memory: 4.65 MB -BaekjoonHub

---
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.cpp
@@ -0,0 +1,53 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int dx[4] = {1, -1, 0, 0};
+int dy[4] = {0, 0, 1, -1};
+int visited[101][101];
+
+vector<int> solution(vector<string> maps) {
+    vector<int> answer;
+    
+    int n = maps.size();
+    int m = maps[0].size();
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (maps[i][j] != 'X' && visited[i][j] == 0) {
+                queue<pair<int, int>> q;
+                q.push({i, j});
+                visited[i][j] = 1;
+                int sum = (maps[i][j] - '0');
+                
+                while(!q.empty()) {
+                    int curx = q.front().first;
+                    int cury = q.front().second;
+                    q.pop();
+                    
+                    for (int dir = 0; dir < 4; dir++) {
+                        int nx = curx + dx[dir];
+                        int ny = cury + dy[dir];
+                        
+                        if (nx < 0 || nx >= n || ny < 0 || ny >= m)
+                            continue;
+                        if (maps[nx][ny] == 'X' || visited[nx][ny] == 1)
+                            continue;
+                        q.push({nx, ny});
+                        visited[nx][ny] = 1;
+                        sum += (maps[nx][ny] - '0');
+                    }
+                }
+                answer.push_back(sum);
+            }
+        }
+    }
+    if (answer.size() == 0)
+        answer.push_back(-1);
+    sort (answer.begin(), answer.end());
+    
+    return answer;
+}
