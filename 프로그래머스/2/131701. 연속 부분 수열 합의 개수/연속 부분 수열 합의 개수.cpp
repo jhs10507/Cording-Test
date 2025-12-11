493106624b7ee10ebc4364e35c46f34a6e74b3ae
Hyunsu Jung <jhs880514@naver.com>
2025-12-11 09:45:30 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 381.18 ms, Memory: 22.8 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
@@ -1,22 +1,19 @@
-#include <string>
 #include <vector>
 #include <set>
-
 using namespace std;
 
 int solution(vector<int> elements) {
     int answer = 0;
-    set<int> temp;
+    set<int> seq;
     
     for (int i = 0; i < elements.size(); ++i) {
         int sum = 0;
-        
         for (int j = i; j < i + elements.size(); ++j) {
-            int index = j % elements.size();
-            sum += elements[index];
-            temp.insert(sum);
+            int idx = j % elements.size();
+            sum += elements[idx];
+            seq.insert(sum);
         }
     }
-    answer = temp.size();
+    answer = seq.size();
     return answer;
 }
