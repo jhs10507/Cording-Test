31f8fc5ad6827c41edeaea210e008a88db9a0c0a
Hyunsu Jung <jhs880514@naver.com>
2025-12-11 09:41:18 +0900

    [level 2] Title: 괄호 회전하기, Time: 4.35 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cpp
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cpp
@@ -6,30 +6,26 @@ using namespace std;
 int solution(string s) {
     int answer = 0;
     
-    for (int i = 0; i < s.size(); ++i) 
-    {
+    for (int i = 0; i < s.size(); ++i) {
         vector<char> check;
         bool isCorrect = true;
-        for (int j = i; j < i + s.size(); ++j) 
-        {
+        for (int j = i; j < i + s.size(); ++j) {
             int idx = j % s.size();
-            
-            if (s[idx] == '(' || s[idx] == '{' || s[idx] == '[') 
+            if (s[idx] == '(' || s[idx] == '{' || s[idx] == '[')
                 check.push_back(s[idx]);
-            else 
-            { 
+            else {
                 if (check.empty() || 
                     (s[idx] == ')' && check.back() != '(') ||
                     (s[idx] == '}' && check.back() != '{') ||
-                    (s[idx] == ']' && check.back() != '[')) 
-                {
+                    (s[idx] == ']' && check.back() != '[')) {
                     isCorrect = false;
                     break;
                 }
                 check.pop_back();
             }
         }
-        if (isCorrect && check.empty()) answer++;
+        if(isCorrect && check.empty()) answer++;
     }
+    
     return answer;
 }
